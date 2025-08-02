c5c84328167524cccd5c2e195253c2f4cbda0c63
aimboot-dt <lushdudeaimboot@gmail.com>
2025-08-02 17:12:09 -0500

    Create mkdir_p.c
    so that i don't get injection hack risks

---
+++ src/mkdir_p.c
@@ -0,0 +1,30 @@
+#include <libgen.h>
+#include <string.h>
+#include <limits.h>
+
+int mkdir_p(const char* path) {
+  char tmp[PATH_MAX];
+  char *p = NULL;
+  size_t len;
+
+  snprintf(tmp, sizeof(tmp), "%s", path);
+  len = strlen(tmp);
+  if ( tmp[len-1] == '/')  {tmp[len-1] = 0;}
+  
+  for (p = tmp + 1; *p; p++){
+    if (*p == '/') {
+      *p = 0;
+      if (mkdir(tmp,0755) && errno != EEXIST){
+        perror("mkdir_p failed");
+        return 1;
+      }
+    *p = '/'
+    }
+  }
+  if (mkdir(tmp, 0755) && errno != EEXIST) {
+    perror("mkdir_p final failed");
+    return 1;
+  }
+
+  return 0;
+}
