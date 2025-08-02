a7ac804a85680123a53baeb0a8566bae867610e5
aimboot-dt <lushdudeaimboot@gmail.com>
2025-08-01 19:39:27 -0500

    Update refresh.c
    Fixed “folder” variable to use getenv instead of tilde expansion

--- src/refresh.c
+++ src/refresh.c
@@ -5,10 +5,12 @@
 #include <stdio.h>
 #include "refresh.h"
 
+const char *home = getenv("HOME");
+
 void refresh_cache_all() {
     for (int i = 0; i < repo_count; ++i) {
         char folder[128];
-        snprintf(folder, sizeof(folder), "~/.local/gralona/cache/%s", repos[i].name);
+        snprintf(folder, sizeof(folder), "%s/.local/gralona/cache/%s", home, repos[i].name);
 
         // Make folder if it doesn't exist
         if (access(folder, F_OK) != 0) {
